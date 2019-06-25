a3c2de2410907cfcd56af7786821df436db92b24
David Wade <d_wade1@icloud.com>
2019-06-25 19:09:31 +1000

    initial seeds

--- ba-server/db/schema.rb
+++ ba-server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_25_082724) do
+ActiveRecord::Schema.define(version: 2019_06_25_083821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2019_06_25_082724) do
     t.text "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_admin"
   end
 
 end
--- ba-server/db/seeds.rb
+++ ba-server/db/seeds.rb
@@ -6,35 +6,50 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# User.destroy_all
+User.destroy_all
 
-# u1 = User.create! name:'', email: '', password_digest: ''
-# u2 = User.create! name:'', email: '', password_digest: ''
-# u3 = User.create! name:'', email: '', password_digest: ''
-# u4 = User.create! name:'', email: '', password_digest: ''
+u1 = User.create! name:'Harpo', email: 'harpo@marx.co', password: 'chicken', is_admin: true
+u2 = User.create! name:'Groucho', email: 'groucho@marx.co', password: 'chicken', is_admin: false
+u3 = User.create! name:'Chico', email: 'chico@marx.co', password: 'chicken', is_admin: false
 
-# puts "Created #{User.all.length} users"
-# puts "done"
-
-
-# Airplane.destroy_all
-
-# a1 = Airplane.create! name: , rows: , columns:
-# a2 = Airplane.create! name: , rows: , columns:
-# a3 = Airplane.create! name: , rows: , columns:
-# a4 = Airplane.create! name: , rows: , columns:
-
-# puts "Created #{Airplane.all.length} Airplanes"
-# puts "done"
+puts "Created #{User.all.length} users"
+puts "done"
 
 
+Airplane.destroy_all
 
-# Flights.destroy_all
-#
-# f1 = Flights.create! flight_number: , date: , to: '', from: '', airplane_id:
-# f2 = Flights.create! flight_number: , date: , to: '', from: '', airplane_id:
-# f3 = Flights.create! flight_number: , date: , to: '', from: '', airplane_id:
-# f4 = Flights.create! flight_number: , date: , to: '', from: '', airplane_id:
-#
-# puts "Created #{Flights.all.length} Flights"
-# puts "done"
+a1 = Airplane.create! name: 747, rows: 18, columns: 4
+a2 = Airplane.create! name: 707, rows: 20, columns: 4
+a3 = Airplane.create! name: 777, rows: 22, columns: 4
+a4 = Airplane.create! name: 737, rows: 16, columns: 4
+
+puts "Created #{Airplane.all.length} Airplanes"
+puts "done"
+
+
+
+Flight.destroy_all
+
+f1 = Flight.create! flight_number: 100, date: Date.strptime('27-06-2019', '%d-%m-%Y'), to: 'SYD', from: 'YVR'
+f2 = Flight.create! flight_number: 200, date: Date.strptime('28-06-2019', '%d-%m-%Y'), to: 'LAX', from: 'JFK'
+f3 = Flight.create! flight_number: 300, date: Date.strptime('30-06-2019', '%d-%m-%Y'), to: 'LGS', from: 'NYO'
+f4 = Flight.create! flight_number: 400, date: Date.strptime('29-06-2019', '%d-%m-%Y'), to: 'AMS', from: 'BUF'
+
+puts "Created #{Flight.all.length} Flights"
+puts "done"
+
+
+Reservation.destroy_all
+
+r1 = Reservation.create! seat: '10B'
+r2 = Reservation.create! seat: '15A'
+r3 = Reservation.create! seat: '20C'
+r4 = Reservation.create! seat: '1A'
+r5 = Reservation.create! seat: '7D'
+r6 = Reservation.create! seat: '11A'
+r7 = Reservation.create! seat: '12B'
+r8 = Reservation.create! seat: '7C'
+r9 = Reservation.create! seat: '2C'
+
+puts "Created #{Reservation.all.length} Reservations"
+puts "done"
