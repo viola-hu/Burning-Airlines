18de910b1601135c964ae517310b2e4f43976ac6
Viola Hu <lovelyviolaat@hotmail.com>
2019-06-26 16:04:59 +1000

    finished Flight controller and only commit related flight_controller, views/flights/, and Gemfile, Gemfile.lock

--- ba-server/app/controllers/flights_controller.rb
+++ ba-server/app/controllers/flights_controller.rb
@@ -1,7 +1,18 @@
 class FlightsController < ApplicationController
+  # security check! only admin can touch this controller's actions, def is in the application.html
+
+  # before_action :check_if_admin
+
+
+  def index
+    @flights= Flight.all.order(date: :asc)
+  end
+
+
   def new
     @flight = Flight.new
   end
+
 
   def create
     @flight = Flight.new params_flight
@@ -16,24 +27,32 @@ class FlightsController < ApplicationController
   end
 
 
-  def index
-
-    @flight 
-
-
-  end
-
-  def show
-  end
-
   def edit
+    @flight = Flight.find params[:id]
   end
+
 
   def update
+    @flight = Flight.find params[:id]
+
+    if @flight.update params_flight
+      redirect_to flights_path
+    else
+      flash.now[:errors] = @flight.errors.full_messages
+      render :edit
+    end
   end
 
+
   def destroy
+    Flight.destroy params[:id]
+    redirect_to flights_path
   end
+
+
+
+  private
+
 
   def params_flight
     params.require(:flight).permit(:flight_number, :date, :to, :from, :airplane_id)
