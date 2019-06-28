54d65f7426a84f0899b46fe13cc630877573fba7
Viola Hu <lovelyviolaat@hotmail.com>
2019-06-28 10:23:42 +1000

    edited SeatPlan and tested locally fine

--- ba-frontend/src/components/SeatPlan.jsx
+++ ba-frontend/src/components/SeatPlan.jsx
@@ -1,22 +1,107 @@
 import React, {Component} from 'react';
 
-
 class SeatPlan extends Component {
 
-render(){
-    console.log('this.props.seat', this.props.seat);
+  onClickSeat(seat){
+    this.props.onClickSeat(seat);
+  }
 
-  return(
-    <div>
-      {this.props.seat.map( seat =>
-      <div>
-        {seat}
+  render(){
+
+    if (this.props.seat.length <= 0) {
+      return(
+        <div>
+          Loading...
+        </div>
+      )
+    }
+
+    return(
+      <div className="container">
+
+        <table className="grid">
+
+          <tbody>
+            <tr>
+            {this.props.seat.map(seat =>
+              <td
+              key={seat}
+              onClick={ event => this.onClickSeat(seat)}
+              >
+
+                {this.props.taken.indexOf(seat) > -1 ? 'X':'avail'}
+
+              </td>
+            )}
+            </tr>
+
+          </tbody>
+        </table>
       </div>
-      )}
-    </div>
 
-  )
+
+    )
+
+
+
+  }
+
+
+
 }
-} // end SeatPlan
 
-export default SeatPlan;
+export default SeatPlan
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, {Component} from 'react';
+//
+//
+// class SeatPlan extends Component {
+//
+// render(){
+//     console.log('this.props.seat', this.props.seat);
+//
+//   return(
+//     <div>
+//       {this.props.seat.map( seat =>
+//       <div>
+//         {seat}
+//       </div>
+//       )}
+//     </div>
+//
+//   )
+// }
+// } // end SeatPlan
+//
+// export default SeatPlan;
